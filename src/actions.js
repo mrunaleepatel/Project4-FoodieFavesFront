b1c7d1a4725c5bd1694c2315a2c5d5d0817c8f25
Mrunalee Patel <mrunaleepatel997@gmail.com>
2023-09-20 21:23:23 -0500

    updated actions

--- src/actions.js
+++ src/actions.js
@@ -6,7 +6,7 @@ export const createAction = async({request}) => {
         img: formData.get('img'),
         url: formData.get('url'),
         author: formData.get('author'),
-        course: formData.get('course'),
+        course: formData.get('course')
     }
     await fetch(url, {
         method: 'POST',
@@ -18,4 +18,32 @@ export const createAction = async({request}) => {
     })
     
     return redirect("/")
+}
+
+// Update action
+export const updateAction = async({request, params}) => {
+    // get the id from params
+    const id = params.id
+    // parse out the form data
+    const formData = await request.formData();
+    // construct the updated todo
+    const updatedRecipe = {
+        title: formData.get('title'),
+        img: formData.get('img'),
+        url: formData.get('url'),
+        author: formData.get('author'),
+        course: formData.get('course')
+    }
+
+    // make a request to update a todo
+    await fetch(url + id, {
+        method: "put",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(updatedRecipe)
+    })
+
+    // redirect to the show page
+    return redirect(`/post/${id}`)
 }
