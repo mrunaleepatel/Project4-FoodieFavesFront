6e15ce2e29f74b654d877647b6649edfbade8ce6
Mrunalee Patel <mrunaleepatel997@gmail.com>
2023-09-23 13:28:51 -0500

    styling

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -1,21 +1,44 @@
-import Post from "../components/Post";
-import { Form, useLoaderData } from "react-router-dom";
+// import Post from "../components/Post";
+import { Form, useLoaderData, Link } from "react-router-dom";
+import "../styles.css";
 
 function Index (props){
     const recipes = useLoaderData()
+    console.log(recipes)
     return <>
-        <div style={{textAlign: 'center'}}>
-            <h2>Create a Recipe</h2>
-            <Form method="post" action="/create">
-            <input type="text" name="title" placeholder="Title"/>
-            <input type="text" name="img" placeholder="Img"/>
-            <input type="text" name="url" placeholder="Url"/>
-            <input type="text" name="author" placeholder="Author"/>
-            <input type="text" name="course" placeholder="Course"/>
-            <button>Create a new Recipe</button>
-        </Form>
+        <div style={{ textAlign: 'center' }}>
+    <h2>Create a Recipe</h2>
+    <Form method="post" action="/create">
+        <input type="text" name="title" placeholder="Title" />
+        <input type="text" name="img" placeholder="Img" />
+        <input type="text" name="url" placeholder="Url" />
+        <input type="text" name="author" placeholder="Author" />
+        <label htmlFor="courseDropdown">Select a course:</label>
+        <select name="courseDropdown" id="courseDropdown" className="custom-dropdown">
+        <option value="breakfast">Breakfast</option>
+        <option value="lunch">Lunch</option>
+        <option value="dinner">Dinner</option>
+        </select>
+        <button style={{ display: 'block', margin: '0 auto' }}>Create a new Recipe</button>
+    </Form>
+    </div>
+        {/* {recipes.map((recipe) => <Post key={recipe.id} post={recipe}/>)} */}
+        
+        <div className="image-grid">
+        {recipes.map((recipe) => (
+            <div className="grid-item" key={recipe.id}>
+            <div className="grid-item-content">
+                <img src={recipe.img} alt={recipe.title} />
+                <h3>{recipe.title}</h3>
+            </div>
+            <Link to={`post/${recipe.id}`}>
+                <div className="image-button">
+                <button>Update Recipe</button>
+                </div>
+            </Link>
+            </div>
+    ))}
         </div>
-        {recipes.map((recipe) => <Post key={recipe.id} post={recipe}/>)}
     </>
 }
 
--- src/pages/Show.js
+++ src/pages/Show.js
@@ -1,36 +1,39 @@
 import {Link, useLoaderData, Form} from 'react-router-dom';
+import "../styles.css";
 
 function Show (props){
     const post = useLoaderData()
     const div = {
-        textAlign: "center",
-        border: "3px solid green",
-        width: "80%",
-        margin: "30px auto"
+        // textAlign: "center",
+        // border: "3px solid green",
+        // width: "80%",
+        // margin: "30px auto"
     }
     return (
 
          <div style={div}>
-            <h1>{post.title}</h1>
-            <h2>{post.img}</h2>
-            <h2>{post.url}</h2>
-            <h2>{post.author}</h2>
-            <h2>{post.course}</h2>
             <div style={{textAlign: "center"}}>
-                <h2>Update Recipe</h2>
+                <h2>Update {post.title}</h2>
                 <Form method="post" action={`/update/${post.id}`}>
                 <input type="text" name="title" placeholder="Title" defaultValue={post.title}/>
                 <input type="text" name="img" placeholder="Img" defaultValue={post.img}/>
                 <input type="text" name="url" placeholder="Url" defaultValue={post.url}/>
                 <input type="text" name="author" placeholder="Author" defaultValue={post.author}/>
-                <input type="text" name="course" placeholder="Course" defaultValue={post.course}/>
+                <label htmlFor="courseDropdown">Select a course:</label>
+                <select name="courseDropdown" id="courseDropdown" className="custom-dropdown">
+                <option value="breakfast">Breakfast</option>
+                <option value="lunch">Lunch</option>
+                <option value="dinner">Dinner</option>
+                </select>
+                <div className="show-button">
                 <button>Update Recipe</button>
+                </div>
                 </Form>
                 <Form method="post" action={`/delete/${post.id}`}>
-                    <button>Delete Todo</button>
+                    <button>Delete Recipe</button>
                 </Form>
             </div>
-            <Link to="/">Back to Index</Link>
+            <Link to="/" className="index-button">Back to Index</Link>
             </div>
     );
 }
